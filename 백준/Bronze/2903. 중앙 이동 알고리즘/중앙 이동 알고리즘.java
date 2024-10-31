21a72e682f243274eab5ed78e54cb1b7085663e9
자기전양치질 <68370338+choijinsung05@users.noreply.github.com>
2024-11-01 00:49:43 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 108 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int answer = 1 + (int) (Math.pow(2, N));
+        answer = answer * answer;
+        System.out.println(answer);
+    }
+}
+
+//3 5 9 17 33
+//9 25 81 289
