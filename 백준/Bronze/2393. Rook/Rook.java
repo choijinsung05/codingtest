54a850e084d768083b17713b942f6e1c399b51d6
자기전양치질 <68370338+choijinsung05@users.noreply.github.com>
2024-11-01 00:27:22 +0900

    [Bronze V] Title: Rook, Time: 100 ms, Memory: 13924 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("  ___  ___  ___\n")
+          .append("  | |__| |__| |\n")
+          .append("  |           |\n")
+          .append("   \\_________/\n")
+          .append("    \\_______/\n")
+          .append("     |     |\n")
+          .append("     |     |\n")
+          .append("     |     |\n")
+          .append("     |     |\n")
+          .append("     |_____|\n")
+          .append("  __/       \\__\n")
+          .append(" /             \\\n")
+          .append("/_______________\\");
+
+        System.out.println(sb);
+
+    }
+}
